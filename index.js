edba0c3e8f3fc390dd64883519e217c3519ed652
RAMU ANIDHARAN <ramuasan.anidharan@usda.gov>
2021-07-14 16:15:55 -0400

    Commit to main

--- index.js
+++ index.js
@@ -222,7 +222,8 @@ function getValuesFromPayload(payload,env)
 
         env : {
             organization: env.ado_organization != undefined ? env.ado_organization : "",
-            orgUrl: env.ado_organization != undefined ? "https://devops.azurecloudgov.us/" + env.ado_organization : "",
+            // orgUrl: env.ado_organization != undefined ? "https://devops.azurecloudgov.us/" + env.ado_organization : "",
+            orgUrl: env.ado_organization != undefined ? "https://devops.azurecloudgov.us/DefaultCollection" + env.ado_organization : "",
             // orgUrl: env.ado_organization != undefined ? "https://devops.azurecloudgov.us/DefaultCollection" + env.ado_organization : "",
             adoToken: env.ado_token != undefined ? env.ado_token : "",
             project: env.ado_project != undefined ? env.ado_project : "",
